bb2de7ce81b4255f037ad2c374ff783c31260ecc
Mateusz Grzymowicz <mateusz.grzymowicz@gmail.com>
2018-09-10 17:55:00 +0200

    fixed dimension of copyrights

--- src/components/Footer/FooterComponents/FooterAboutMe.js
+++ src/components/Footer/FooterComponents/FooterAboutMe.js
@@ -11,13 +11,11 @@ class FooterAboutMe extends Component {
                     nagrywanie obrazu w jakości 4K. Zasięg naszych usług jest praktycznie nieograniczony, działamy
                     głównie
                     na terenie całego Trójmiasta oraz w granicach województwa pomorskiego. Zlecenie wykonujmy możliwie
-                    szybko i w konkurencyjnych cenach po uzgodnieniu szczegułów z klientem. Angażując się w każdy
+                    szybko i w konkurencyjnych cenach po uzgodnieniu szczegółów z klientem. Angażując się w każdy
                     projekt
                     proponujemy ciekawe ujęcia i rozwiązania przy realizacji każdego projektu. Szybkość realizacji to
-                    nasza
-                    mocna strona, możliwość otrzymania materiału nawet na drugi dzień po wykonaniu zlecenia. Zdjęcia
-                    oraz
-                    nagrania przekazujemy w surowym formacie lub po odpowiedniej obróbce.
+                    nasza mocna strona, istnieje możliwość otrzymania materiału nawet na drugi dzień po wykonaniu
+                    zlecenia. Zdjęcia oraz nagrania przekazujemy w surowym formacie lub po odpowiedniej obróbce.
                 </p>
                 <p className="footer-aboutMe-two">
                     Zapraszamy do kontaktu.
