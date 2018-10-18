c21252410ddefa5c6e8fbef1bafe09e6f8c5ef61
Mateusz Grzymowicz <mateusz.grzymowicz@gmail.com>
2018-10-18 12:02:39 +0200

    fixed button to work with mozzila/ie

--- src/components/Header/HeaderComponents/HeaderTitle.js
+++ src/components/Header/HeaderComponents/HeaderTitle.js
@@ -7,7 +7,9 @@ class HeaderTitle extends Component {
             <div className="header-title">
                 <h2 className="header-title-header">MaxDron</h2>
                 <p className="header-title-paragraph">latamy dla najlepszych</p>
-                <button className="header-title-btn"><a href="#contact">O nas</a></button>
+                <div className="header-title-btn">
+                    <a href="#contact">O nas</a>
+                </div>
             </div>
 
         );
