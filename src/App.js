2391aac4f2a05910660b9458221d9d8aa395dd38
Mateusz Grzymowicz <mateusz.grzymowicz@gmail.com>
2018-10-30 20:59:56 +0100

    added pricelist

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React, {Component} from 'react';
 
-import {Jumbotron} from 'react-bootstrap';
+import {Jumbotron, Image } from 'react-bootstrap';
 import {Parallax} from 'react-parallax';
 
 import Header from "./components/Header/Header";
@@ -12,6 +12,9 @@ import Footer from "./components/Footer/Footer";
 import transition1 from './img/transition1.jpg';
 import headerPhoto from './img/header.jpg';
 import parallaxBtn from "./img/gallery.png";
+
+import pricePhoto from './img/photo.png';
+import priceVideo from './img/video.png';
 
 class App extends Component {
     render() {
@@ -38,6 +41,16 @@ class App extends Component {
                         strength={300}
                     >
                         <div className="parallax">
+                            <div className="pricelistContainer">
+                                <div className="pricelistPhoto">
+                                <Image src={pricePhoto} alt="photo icon"></Image>
+                                <h3>Nasz zdjecia zaczynaja sie od</h3>
+                                </div>
+                                <div className="pricelistVideo">
+                                <Image src={priceVideo} alt="photo icon"></Image>
+                                <h3>Nasz filmy zaczynaja sie od</h3>
+                                </div>
+                            </div>
                             <div className="parallax-wrapper">
                                 <h2>Nasza galeria</h2>
                                 <button>
@@ -45,9 +58,6 @@ class App extends Component {
                                         <img src={parallaxBtn} alt="parallaxBtn"/>
                                     </a>
                                 </button>
-                                <h2>Nasz cennik</h2>
-                                <h3>Fotografie zaczynają się od 100zł</h3>
-                                <h3>Filmy zaczynają się od 150zł</h3>
                             </div>
                         </div>
                     </Parallax>
