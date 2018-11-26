cfae4cff061d82fdcd205c3e03b46e35189b41ef
Mateusz Grzymowicz <mateusz.grzymowicz@gmail.com>
2018-11-26 11:43:48 +0100

    changed footer website

--- src/components/Footer/FooterComponents/FooterCopyrights.js
+++ src/components/Footer/FooterComponents/FooterCopyrights.js
@@ -8,7 +8,7 @@ import { Row, Col} from 'react-bootstrap';
             <Row className="show-grid footer">
                 <Col xs={12} sm={12} className="footer-copyrights">
                     <span>&copy; 2018 Michał Renka - MaxDron All Rights Reserved.</span>
-                    <span>Designed and implemented <a href="https://zirek90.github.io/Mateusz-Grzymowicz-Portfolio/">Mateusz Grzymowicz</a></span>
+                    <span>Designed and implemented <a href="https://mateusz-grzymowicz.herokuapp.com/">Mateusz Grzymowicz</a></span>
                 </Col>
             </Row>
         );
