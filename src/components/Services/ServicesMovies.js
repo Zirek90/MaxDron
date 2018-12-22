8ee0247af9510b3dafa2ce8e02e9a250e8b781cc
Mateusz Grzymowicz <mateusz.grzymowicz@gmail.com>
2018-12-22 16:25:16 +0100

    moved movies imports to separate database

--- src/components/Services/ServicesMovies.js
+++ src/components/Services/ServicesMovies.js
@@ -3,28 +3,7 @@ import React from 'react';
 import { Grid, Row } from 'react-bootstrap';
 
 import ServicesComponentsMovie from "./ServicesComponents/ServicesComponentsMovie";
-const ServicesPhotoRow = [
-    {
-        video: "jUygnVNGE00",
-        title: "Olivia Business Centre - Olivia Star 180m (06.2018) MaxDron"
-    },
-    {
-        video: "JgwNAw1QkLA",
-        title: "Arktyczna Gdynia 2018 4K - MaxDron"
-    },
-    {
-        video: "NZjHgWjUU8Y",
-        title: "Olivia Business Centre - Olivia Star 180m (03.2018) MaxDron"
-    },
-    {
-        video: "0YqbS9xNPik",
-        title: "Bajecznie białe Orłowo 2018 4k - MaxDron"
-    },
-    {
-        video: "pWEexDZ2fBc",
-        title: "Podsumowanie roku 2017 4K - MaxDron"
-    }
-];
+import ServicesMovieRow from '../../database/databaseMovies';
 
     const ServicesMovies = () => {
         return (
@@ -33,7 +12,7 @@ const ServicesPhotoRow = [
                 <Grid className="text-center services-container">
                     <Row className="show-grid text-center services-movie">
 
-                        {ServicesPhotoRow.map((e, i) =>
+                        {ServicesMovieRow.map((e, i) =>
                             <ServicesComponentsMovie
                                 video={e.video}
                                 title={e.title}
---
+++ src/database/databaseMovies.js
@@ -0,0 +1,23 @@
+const ServicesMovieRow = [
+    {
+        video: "jUygnVNGE00",
+        title: "Olivia Business Centre - Olivia Star 180m (06.2018) MaxDron"
+    },
+    {
+        video: "JgwNAw1QkLA",
+        title: "Arktyczna Gdynia 2018 4K - MaxDron"
+    },
+    {
+        video: "NZjHgWjUU8Y",
+        title: "Olivia Business Centre - Olivia Star 180m (03.2018) MaxDron"
+    },
+    {
+        video: "0YqbS9xNPik",
+        title: "Bajecznie białe Orłowo 2018 4k - MaxDron"
+    },
+    {
+        video: "pWEexDZ2fBc",
+        title: "Podsumowanie roku 2017 4K - MaxDron"
+    }
+];
+export default ServicesMovieRow
