2a57744f0b34b04ca5e405bb0739a08afa1de47e
Mateusz Grzymowicz <mateusz.grzymowicz@gmail.com>
2019-01-21 13:12:07 +0100

    added photos to gdansk section

--- src/database/databasePhotos.js
+++ src/database/databasePhotos.js
@@ -1,11 +1,11 @@
-import project1 from '../img/Project1.jpg'
-import project2 from '../img/Project2.jpg'
-import project3 from '../img/Project3.jpg'
-import project4 from '../img/Project4.jpg'
-import project5 from '../img/Project5.jpg'
-import project6 from '../img/Project6.jpg'
-import project7 from '../img/Project7.jpg'
-import project8 from '../img/Project8.jpg'
+import project1 from '../img/Services/Project1.jpg'
+import project2 from '../img/Services/Project2.jpg'
+import project3 from '../img/Services/Project3.jpg'
+import project4 from '../img/Services/Project4.jpg'
+import project5 from '../img/Services/Project5.jpg'
+import project6 from '../img/Services/Project6.jpg'
+import project7 from '../img/Services/Project7.jpg'
+import project8 from '../img/Services/Project8.jpg'
 
 const ServicesPhotoRow = [
     {
--- src/database/galleryDatabase.js
+++ src/database/galleryDatabase.js
@@ -1,11 +1,67 @@
-import project1 from '../img/Project1.jpg'
-import project2 from '../img/Project2.jpg'
-import project3 from '../img/Project3.jpg'
-import project4 from '../img/Project4.jpg'
-import project5 from '../img/Project5.jpg'
-import project6 from '../img/Project6.jpg'
-import project7 from '../img/Project7.jpg'
-import project8 from '../img/Project8.jpg'
+import project1 from '../img/Services/Project1.jpg'
+import project2 from '../img/Services/Project2.jpg'
+import project3 from '../img/Services/Project3.jpg'
+import project4 from '../img/Services/Project4.jpg'
+import project5 from '../img/Services/Project5.jpg'
+import project6 from '../img/Services/Project6.jpg'
+import project7 from '../img/Services/Project7.jpg'
+import project8 from '../img/Services/Project8.jpg'
+
+// Gdańsk
+import gdansk1 from '../img/Gallery/Gdansk/skompresowane/photo1.JPG';
+import gdansk2 from '../img/Gallery/Gdansk/skompresowane/photo2.JPG';
+import gdansk3 from '../img/Gallery/Gdansk/skompresowane/photo3.JPG';
+import gdansk4 from '../img/Gallery/Gdansk/skompresowane/photo4.JPG';
+import gdansk5 from '../img/Gallery/Gdansk/skompresowane/photo5.JPG';
+import gdansk6 from '../img/Gallery/Gdansk/skompresowane/photo6.JPG';
+import gdansk7 from '../img/Gallery/Gdansk/skompresowane/photo7.JPG';
+import gdansk8 from '../img/Gallery/Gdansk/skompresowane/photo8.JPG';
+import gdansk9 from '../img/Gallery/Gdansk/skompresowane/photo9.JPG';
+import gdansk10 from '../img/Gallery/Gdansk/skompresowane/photo10.JPG';
+import gdansk11 from '../img/Gallery/Gdansk/skompresowane/photo11.JPG';
+import gdansk12 from '../img/Gallery/Gdansk/skompresowane/photo12.JPG';
+import gdansk13 from '../img/Gallery/Gdansk/skompresowane/photo13.JPG';
+import gdansk14 from '../img/Gallery/Gdansk/skompresowane/photo14.JPG';
+import gdansk15 from '../img/Gallery/Gdansk/skompresowane/photo15.JPG';
+import gdansk16 from '../img/Gallery/Gdansk/skompresowane/photo16.JPG';
+import gdansk17 from '../img/Gallery/Gdansk/skompresowane/photo17.JPG';
+import gdansk18 from '../img/Gallery/Gdansk/skompresowane/photo18.JPG';
+import gdansk19 from '../img/Gallery/Gdansk/skompresowane/photo19.JPG';
+import gdansk20 from '../img/Gallery/Gdansk/skompresowane/photo20.JPG';
+import gdansk21 from '../img/Gallery/Gdansk/skompresowane/photo21.JPG';
+import gdansk22 from '../img/Gallery/Gdansk/skompresowane/photo22.JPG';
+import gdansk23 from '../img/Gallery/Gdansk/skompresowane/photo23.JPG';
+import gdansk24 from '../img/Gallery/Gdansk/skompresowane/photo24.JPG';
+import gdansk25 from '../img/Gallery/Gdansk/skompresowane/photo25.JPG';
+import gdansk26 from '../img/Gallery/Gdansk/skompresowane/photo26.JPG';
+import gdansk27 from '../img/Gallery/Gdansk/skompresowane/photo27.JPG';
+import gdansk28 from '../img/Gallery/Gdansk/skompresowane/photo28.JPG';
+import gdansk29 from '../img/Gallery/Gdansk/skompresowane/photo29.JPG';
+import gdansk30 from '../img/Gallery/Gdansk/skompresowane/photo30.JPG';
+import gdansk31 from '../img/Gallery/Gdansk/skompresowane/photo31.JPG';
+import gdansk32 from '../img/Gallery/Gdansk/skompresowane/photo32.JPG';
+import gdansk33 from '../img/Gallery/Gdansk/skompresowane/photo33.JPG';
+import gdansk34 from '../img/Gallery/Gdansk/skompresowane/photo34.JPG';
+import gdansk35 from '../img/Gallery/Gdansk/skompresowane/photo35.JPG';
+import gdansk36 from '../img/Gallery/Gdansk/skompresowane/photo36.JPG';
+import gdansk37 from '../img/Gallery/Gdansk/skompresowane/photo37.JPG';
+import gdansk38 from '../img/Gallery/Gdansk/skompresowane/photo38.JPG';
+import gdansk39 from '../img/Gallery/Gdansk/skompresowane/photo39.JPG';
+import gdansk40 from '../img/Gallery/Gdansk/skompresowane/photo40.JPG';
+import gdansk41 from '../img/Gallery/Gdansk/skompresowane/photo41.JPG';
+import gdansk42 from '../img/Gallery/Gdansk/skompresowane/photo42.JPG';
+import gdansk43 from '../img/Gallery/Gdansk/skompresowane/photo43.JPG';
+import gdansk44 from '../img/Gallery/Gdansk/skompresowane/photo44.JPG';
+import gdansk45 from '../img/Gallery/Gdansk/skompresowane/photo45.JPG';
+import gdansk46 from '../img/Gallery/Gdansk/skompresowane/photo46.JPG';
+import gdansk47 from '../img/Gallery/Gdansk/skompresowane/photo47.JPG';
+import gdansk48 from '../img/Gallery/Gdansk/skompresowane/photo48.JPG';
+import gdansk49 from '../img/Gallery/Gdansk/skompresowane/photo49.JPG';
+import gdansk50 from '../img/Gallery/Gdansk/skompresowane/photo50.JPG';
+import gdansk51 from '../img/Gallery/Gdansk/skompresowane/photo51.JPG';
+import gdansk52 from '../img/Gallery/Gdansk/skompresowane/photo52.JPG';
+import gdansk53 from '../img/Gallery/Gdansk/skompresowane/photo53.JPG';
+import gdansk54 from '../img/Gallery/Gdansk/skompresowane/photo54.JPG';
 
 const photoGallery = {
     'Gdynia': [
@@ -62,28 +118,166 @@ const photoGallery = {
     ],
     'Gdansk': [
         {
-            photo: project2,
+            photo: gdansk1,
         },
         {
-            photo: project2,
+            photo: gdansk2,
         },
         {
-            photo: project1,
+            photo: gdansk3,
         },
         {
-            photo: project5,
+            photo: gdansk4,
         },
         {
-            photo: project4,
+            photo: gdansk5,
         },
         {
-            photo: project6,
+            photo: gdansk6,
         },
         {
-            photo: project7,
+            photo: gdansk7,
         },
         {
-            photo: project8,
+            photo: gdansk8,
+        },
+        {
+            photo: gdansk9,
+        },
+        {
+            photo: gdansk10,
+        },
+        {
+            photo: gdansk11,
+        },
+        {
+            photo: gdansk12,
+        },
+        {
+            photo: gdansk13,
+        },
+        {
+            photo: gdansk14,
+        },
+        {
+            photo: gdansk15,
+        },
+        {
+            photo: gdansk16,
+        },
+        {
+            photo: gdansk17,
+        },
+        {
+            photo: gdansk18,
+        },
+        {
+            photo: gdansk19,
+        },
+        {
+            photo: gdansk20,
+        },
+        {
+            photo: gdansk21,
+        },
+        {
+            photo: gdansk22,
+        },
+        {
+            photo: gdansk23,
+        },
+        {
+            photo: gdansk24,
+        },
+        {
+            photo: gdansk25,
+        },
+        {
+            photo: gdansk26,
+        },
+        {
+            photo: gdansk27,
+        },
+        {
+            photo: gdansk28,
+        },
+        {
+            photo: gdansk29,
+        },
+        {
+            photo: gdansk30,
+        },
+        {
+            photo: gdansk31,
+        },
+        {
+            photo: gdansk32,
+        },
+        {
+            photo: gdansk33,
+        },
+        {
+            photo: gdansk34,
+        },
+        {
+            photo: gdansk35,
+        },
+        {
+            photo: gdansk36,
+        },
+        {
+            photo: gdansk37,
+        },
+        {
+            photo: gdansk38,
+        },
+        {
+            photo: gdansk39,
+        },
+        {
+            photo: gdansk40,
+        },
+        {
+            photo: gdansk41,
+        },
+        {
+            photo: gdansk42,
+        },
+        {
+            photo: gdansk43,
+        },
+        {
+            photo: gdansk44,
+        },
+        {
+            photo: gdansk45,
+        },
+        {
+            photo: gdansk46,
+        },
+        {
+            photo: gdansk47,
+        },
+        {
+            photo: gdansk48,
+        },
+        {
+            photo: gdansk49,
+        },
+        {
+            photo: gdansk50,
+        },
+        {
+            photo: gdansk51,
+        },
+        {
+            photo: gdansk52,
+        },
+        {
+            photo: gdansk53,
+        },
+        {
+            photo: gdansk54,
         }
     ],
     "All": [
