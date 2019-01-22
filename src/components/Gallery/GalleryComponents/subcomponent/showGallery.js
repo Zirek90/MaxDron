08b6b685b7a748d3eee47e5773d5c6f670ed9a7d
Mateusz Grzymowicz <mateusz.grzymowicz@gmail.com>
2019-01-22 17:21:53 +0100

    fixed loader while photo not ready

--- src/components/Gallery/GalleryComponents/subcomponent/showGallery.js
+++ src/components/Gallery/GalleryComponents/subcomponent/showGallery.js
@@ -1,8 +1,8 @@
-import React, {Component} from 'react';
-import {Col, Image, Modal } from 'react-bootstrap';
+import React, { Component } from 'react';
+import { Col, Image, Modal } from 'react-bootstrap';
 
 
-class ShowGallery extends Component{
+class ShowGallery extends Component {
     constructor(props) {
         super(props);
         this.state = {
@@ -15,43 +15,40 @@ class ShowGallery extends Component{
         this.handleImageLoad = this.handleImageLoad.bind(this);
     }
 
-    handleClose() {
-        this.setState({show: false});
-    }
-
-    handleShow() {
-        this.setState({show: true});
-    }
-
-    
-
-    handleImageLoad() {
-            this.setState({
-                imageStatus: false
-            })
-    }
-
     componentDidMount() {
         this.handleImageLoad();
     }
 
+    handleClose() {
+        this.setState({ show: false });
+    }
+
+    handleShow() {
+        this.setState({ show: true });
+    }
+
+    handleImageLoad() {
+        this.setState({
+            imageStatus: false
+        })
+    }
 
     render() {
-        return(
+        return (
             <Col xs={12} sm={6} md={6} lg={4} className="show-gallery">
-            {!this.state.imageStatus 
-            ?
-            <div>
-                <Image src={this.props.photo} onLoad={this.handleImageLoad} responsive bsStyle="primary"
-                    onClick={this.handleShow}/>
-                <Modal show={this.state.show} onHide={this.handleClose} className="gallery-modal">
-                    <Modal.Body>
-                        <Image src={this.props.photo} rounded responsive/>
-                    </Modal.Body>
-                </Modal>
-                </div>
-                :
-                <div>Im loading</div>
+                {!this.state.imageStatus
+                    ?
+                    <div>
+                        <Image src={this.props.photo} onLoad={this.handleImageLoad} responsive bsStyle="primary"
+                            onClick={this.handleShow} />
+                        <Modal show={this.state.show} onHide={this.handleClose} className="gallery-modal">
+                            <Modal.Body>
+                                <Image src={this.props.photo} rounded responsive />
+                            </Modal.Body>
+                        </Modal>
+                    </div>
+                    :
+                    <div>Im loading</div>
                 }
             </Col>
         )
