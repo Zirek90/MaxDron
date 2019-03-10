32bed09fe2d1da6aef20cf6e38da46329443d232
Mateusz Grzymowicz <mateusz.grzymowicz@gmail.com>
2019-03-10 18:21:25 +0100

    changed hardcoded data to function

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,19 +1,28 @@
-import React from 'react';
+import React, {Component} from 'react';
 import {Grid, Row } from 'react-bootstrap';
 import FooterPhoto from "./FooterComponents/FooterPhoto";
 import FooterSocialMedia from "./FooterComponents/FooterSocialMedia";
 import FooterCopyright from "./FooterComponents/FooterCopyrights";
 
-    const Footer = () => {
+class Footer extends Component {
+    constructor(props) {
+        super(props)
+        this.state = {
+            year: new Date().getFullYear()
+        }
+    }
+
+    render () {        
         return (
             <Grid>
                 <Row className="show-grid footer">
                     <FooterPhoto/>
                     <FooterSocialMedia/>
                 </Row>
-                <FooterCopyright/>
+                <FooterCopyright year={this.state.year}/>
             </Grid>
         );
     }
+}
 
 export default Footer;
--- src/components/Footer/FooterComponents/FooterCopyrights.js
+++ src/components/Footer/FooterComponents/FooterCopyrights.js
@@ -1,11 +1,11 @@
 import React from 'react';
 import { Row, Col} from 'react-bootstrap';
 
-    const FooterCopyright = () => {
+    const FooterCopyright = (props) => {
         return (
             <Row className="show-grid footer">
                 <Col xs={12} sm={12} className="footer-copyrights">
-                    <span>&copy; 2018 Michał Renka - MaxDron All Rights Reserved.</span>
+                    <span>&copy; {props.year} Michał Renka - MaxDron All Rights Reserved.</span>
                     <span>Designed and implemented <a href="https://mateusz-grzymowicz.herokuapp.com/">Mateusz Grzymowicz</a></span>
                 </Col>
             </Row>
